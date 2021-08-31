ab4a09aa08035575f34d8333ca14b3be5c8cb823
Jake Alexander <imjakeal123@gmail.com>
2021-08-31 12:04:39 -0600

    added log and error handler

--- server.js
+++ server.js
@@ -8,11 +8,23 @@ let rollbar = new Rollbar({
     captureUnhandledRejections: true
 })
 
+const students = []
 const app = express()
+
+app.use(rollbar.errorHandler())
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
     rollbar.info('html file served succesfully')
+})
+
+app.post('/api/student', (req, res) => {
+    const {name} = req.body
+    name = name.trim()
+
+    students.push(name)
+    rollbar.log('Studnt added succesfully', {author: 'Scott', type: 'manual entry'})
+    res.status(200).send(students)
 })
 
 const port = process.env.PORT || 4545
