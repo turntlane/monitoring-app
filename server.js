61317e502791129fcfd424162b69a9a6802c286c
Jake Alexander <imjakeal123@gmail.com>
2021-08-31 11:46:54 -0600

    first

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const path = require('path')
+const Rollbar = require('rollbar')
+
+let rollbar = new Rollbar({
+    accessToken: '',
+    captureUncaught: true,
+    captureUnhandledRejections: true
+})
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+    rollbar.info('html file served succesfully')
+})
+
+const port = process.env.PORT || 4545
+
+app.listen(port, () => {
+    console.log('Server up on 4545')
+})
